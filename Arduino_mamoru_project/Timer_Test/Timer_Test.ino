3e22a469a894da73cd517b384d76d618343d7ca9
kumikomi15 <kenta.saito6273@gmail.com>
2023-03-17 13:47:43 +0900

    Timerのテストコードを追加

---
+++ Arduino_mamoru_project/Timer_Test/Timer_Test.ino
@@ -0,0 +1,43 @@
+#include<M5StickCPlus.h>
+
+hw_timer_t * timer;
+volatile uint32_t isrCounter;
+
+int timeSleep;
+int frequency;
+
+
+void onTimer() {
+  isrCounter++;
+  if (isrCounter == 180) {
+    timerEnd(timer);
+    timer = NULL;
+    isrCounter = 0;
+
+    M5.Axp.ScreenBreath(timeSleep);
+    setCpuFrequencyMhz(frequency);
+  }
+}
+
+void setup() {
+  M5.begin();
+  Serial.begin(9600);
+  Serial.println("System Start");
+
+  timer = timerBegin(0, getApbFrequency() / 1000000, true);
+  timerAttachInterrupt(timer, &onTimer, true);
+  timerAlarmWrite(timer, 1000000, true);
+  timerAlarmEnable(timer);
+}
+
+void loop() {
+  M5.update();  // 本体のボタン状態更新
+
+  if (M5.BtnB.wasPressed()) {
+    M5.Lcd.setTextFont(2);
+    M5.Lcd.setTextColor(RED, BLACK);
+    String msg = "BUTOON CLICK";
+    M5.Lcd.print(msg);
+    Serial.print(msg);
+  }
+}
